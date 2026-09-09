a5e93fe2d527f4d41091ad6c79199a940df6e41d
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-09-09 12:50:59 +0530

    add changes

--- client/src/admin/components/ProductForm.jsx
+++ client/src/admin/components/ProductForm.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ProductForm = () => {
+
+    const [highlights, setHighlights] = useState([""])
+    const [information, setinformation] = useState([""])
+    const [benefits, setbenefits] = useState([""])
+    const [specifications, setSpecifications] = useState([""])
+
+    const handleHighlights = () => {
+        setHighlights((prev) => [...prev, ""])
+    }
+
+    const handleInformation = () => {
+        setinformation((prev) => [...prev, ""])
+    }
+
+    const handleBenefits = () => {
+        setbenefits((prev) => [...prev, ""])
+    }
+
+    const handleSpecifications = () => {
+        setSpecifications((prev) => [...prev, ""])
+    }
     return (
         <form className='w-[50%] text-gray-600'>
+
             <div className='mb-3'>
                 <label className='font-semibold'>Choose Category</label>
                 <select type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full'>
@@ -36,53 +58,84 @@ const ProductForm = () => {
 
             <div className='mb-4'>
                 <label className='font-semibold'>Key Highlights</label>
-                <button className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
-                <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-2 border border-[#ccc] w-full' />
+                <button onClick={handleHighlights} type="button" className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
+                {
+                    highlights.map((_, i) => {
+                        return (
+                            <div key={i}>
+                                <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-2 border border-[#ccc] w-full' />
+                            </div>
+                        )
+                    })
+                }
             </div>
 
             <div className='mb-4'>
                 <label className='font-semibold'>Key Information</label>
-                <button className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
-                <div className='mt-2'>
-                    <div>
-                        <span className='text-[#1c9d36] text-sm font-semibold'>Head</span>
-                        <input type='text' className='focus:border-[#1c9d36] outline-none p-1 my-1 border border-[#ccc] w-full' />
-                    </div>
-                    <div className=''>
-                        <span className='text-[#1c9d36] text-sm font-semibold'>Content</span>
-                        <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full' />
-                    </div>
-                </div>
+                <button onClick={handleInformation} type="button" className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
+
+                {
+                    information.map((_, i) => {
+                        return (
+                            <div key={i} className='mt-2'>
+                                <div>
+                                    <span className='text-[#1c9d36] text-sm font-semibold'>Head</span>
+                                    <input type='text' className='focus:border-[#1c9d36] outline-none p-1 my-1 border border-[#ccc] w-full' />
+                                </div>
+                                <div className=''>
+                                    <span className='text-[#1c9d36] text-sm font-semibold'>Content</span>
+                                    <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full' />
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
 
             <div className='mb-4'>
                 <label className='font-semibold'>Key Benefits</label>
-                <button className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
-                <div className='mt-2'>
-                    <div>
-                        <span className='text-[#1c9d36] text-sm font-semibold'>Head</span>
-                        <input type='text' className='focus:border-[#1c9d36] outline-none p-1 my-1 border border-[#ccc] w-full' />
-                    </div>
-                    <div className=''>
-                        <span className='text-[#1c9d36] text-sm font-semibold'>Content</span>
-                        <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full' />
-                    </div>
-                </div>
+                <button onClick={handleBenefits} type="button" className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
+
+                {
+                    benefits.map((_, i) => {
+                        return (
+                            <div key={i} className='mt-2'>
+                                <div>
+                                    <span className='text-[#1c9d36] text-sm font-semibold'>Head</span>
+                                    <input type='text' className='focus:border-[#1c9d36] outline-none p-1 my-1 border border-[#ccc] w-full' />
+                                </div>
+                                <div className=''>
+                                    <span className='text-[#1c9d36] text-sm font-semibold'>Content</span>
+                                    <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full' />
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
 
             <div className='mb-4'>
                 <label className='font-semibold'>Specifications</label>
-                <button className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
-                <div className='mt-2'>
-                    <div>
-                        <span className='text-[#1c9d36] text-sm font-semibold'>Head</span>
-                        <input type='text' className='focus:border-[#1c9d36] outline-none p-1 my-1 border border-[#ccc] w-full' />
-                    </div>
-                    <div className=''>
-                        <span className='text-[#1c9d36] text-sm font-semibold'>Content</span>
-                        <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full' />
-                    </div>
-                </div>
+                <button onClick={handleSpecifications} type="button" className='bg-[#1c9d36] text-white px-2 ml-3'>+</button>
+                {
+                    specifications.map((_, i) => {
+                        return (
+                            <div className='mt-2'>
+                                <div>
+                                    <span className='text-[#1c9d36] text-sm font-semibold'>Head</span>
+                                    <input type='text' className='focus:border-[#1c9d36] outline-none p-1 my-1 border border-[#ccc] w-full' />
+                                </div>
+                                <div className=''>
+                                    <span className='text-[#1c9d36] text-sm font-semibold'>Content</span>
+                                    <input type='text' className='focus:border-[#1c9d36] outline-none p-1 mt-1 border border-[#ccc] w-full' />
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
 
             <div className='mb-3'>
